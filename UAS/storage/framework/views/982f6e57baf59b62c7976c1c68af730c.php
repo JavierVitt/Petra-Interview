a52ecad418971c2e966cd15729031808f1656859
royJuanAndika <118645021+royJuanAndika@users.noreply.github.com>
2024-11-29 12:23:59 +0700

    hapus folder UAS

--- UAS/storage/framework/views/982f6e57baf59b62c7976c1c68af730c.php
+++
@@ -1,7 +0,0 @@
-<nav class="grid grid-cols-3 text-center shadow-md py-4">
-    <img src="webAsset/logo.png" alt="err" width="150" class="ml-10">
-    <div class="grid grid-cols-3 mt-2 text-violet-900 font-bold">
-        <div class=" hover:text-orange-500"><a href="">Add Event</a></div>
-        <div class=" hover:text-orange-500"><a href="">Manage Interview</a></div>
-    </div>
-</nav><?php /**PATH C:\laragon\www\UASWebFrame\UAS\resources\views/navbar/navbar.blade.php ENDPATH**/ ?>
