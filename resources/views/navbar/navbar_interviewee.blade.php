75f7fbe8b23b44570f5ec3c11169eb68aae4f288
royJuanAndika <c14220235@john.petra.ac.id>
2024-12-18 13:35:29 +0700

    benerin logo navbar

--- resources/views/navbar/navbar_interviewee.blade.php
+++ resources/views/navbar/navbar_interviewee.blade.php
@@ -36,7 +36,7 @@
       </div>
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex flex-shrink-0 items-center">
-          <img class="h-8 w-auto" src="{{ asset('storage/logo-navbar.png') }}" alt="Your Company">
+          <img class="h-8 w-auto" src="{{ asset('assets/logo-navbar.png') }}" alt="Peinter">
         </div>
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
