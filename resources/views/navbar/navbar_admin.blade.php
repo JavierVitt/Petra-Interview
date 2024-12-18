97f53959b03322fb432ca0c07c9843d90bdc85b3
JavierVitt <131933914+JavierVitt@users.noreply.github.com>
2024-12-18 13:52:27 +0700

    QA 1.4.1

--- resources/views/navbar/navbar_admin.blade.php
+++ resources/views/navbar/navbar_admin.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-white shadow-md py-4 px-6 flex items-center justify-between">
-    <div class="text-orange-500 font-bold text-2xl">Peinter</div>
+    <div class="text-orange-500 font-bold text-2xl">Peinter Admin Panel</div>
     <ul class="flex items-center space-x-4">
         <li>
             <form action="{{ route('logout.session') }}" method="POST">
